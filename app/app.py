fbb7e31874fd9b9e40996cfc5bcea964d917730c
Kevin Spring <kevinjspring@gmail.com>
2022-12-23 09:37:24 -0600

    updated gitignore

--- app/app.py
+++ app/app.py
@@ -1,18 +1,19 @@
 import streamlit as st
 from img_classification import teachable_machine_classification
+from PIL import Image
 
-st.title("Image Classification")
+st.title("Pneumonia Detection in Chest X-ray EDUCATIONAL USE ONLY")
 st.header("Pneumonia classification from chest X-ray")
 st.text("Upload a Chest X-ray Image for image classification as pneumonia or normal")
 
-uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
+uploaded_file = st.file_uploader("Upload the chest X-ray ...", type=["jpg", "jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded MRI.', use_column_width=True)
+    st.image(image, caption='Uploaded Chest X-ray.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
-    label = teachable_machine_classification(image, 'brain_tumor_classification.h5')
-    if label == 0:
-        st.write("The MRI scan has a brain tumor")
+    label = teachable_machine_classification(image, 'model6.keras')
+    if label > 0.5:
+        st.write("The chest X-ray indicates pneumonia")
     else:
-        st.write("The MRI scan is healthy")
+        st.write("The chest X-ray is normal")
--- app/img_classification.py
+++ app/img_classification.py
@@ -1,27 +1,39 @@
-import keras
-from PIL import Image, ImageOps
-import numpy as np
-
-
 def teachable_machine_classification(img, weights_file):
+
+    import keras
+    from tensorflow.keras.utils import load_img, img_to_array
+    from PIL import Image, ImageOps
+    import numpy as np
+    
     # Load the model
     model = keras.models.load_model(weights_file)
+    
+    #Load the image
+    #image = load_img(img,
+                     #color_mode='grayscale',
+                     #target_size=(128,128))
+    
+    # Construct the tensor that .predict is expecting
+    image = ImageOps.fit(img, (128,128), Image.ANTIALIAS)
+    image = ImageOps.grayscale(image) # grayscale only images
+    image = img_to_array(image) # convert image to array
+    data = np.expand_dims(image, axis=0)#/255 # normalize
 
     # Create the array of the right shape to feed into the keras model
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = img
-    #image sizing
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    #data = np.ndarray(shape=(1, 128, 128, 1), dtype=np.float32)
+    #image = img
+    # image sizing
+    #size = (128, 128)
+    #image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
     #turn the image into a numpy array
-    image_array = np.asarray(image)
+    #image_array = np.asarray(image)
     # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    #normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
     # Load the image into the array
-    data[0] = normalized_image_array
+    #data[0] = normalized_image_array
 
-    # run the inference
-    prediction = model.predict(data)
-    return np.argmax(prediction) # return position of the highest probability
+    # get the prediction
+    prediction = model.predict(data, verbose=0)
+    return prediction #np.argmax(prediction) # return position of the highest probability
